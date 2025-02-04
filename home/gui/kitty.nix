9853878b63575f094f5ef0e2cf4c91be4ea2b46a
jazzpi <jasper@mezzo.de>
2025-02-04 01:35:28 +0100

    Add kitty

---
+++ home/gui/kitty.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  lib,
+  config,
+  rootPath,
+  ...
+}:
+{
+  options.j.gui.kitty = {
+    enable = lib.mkEnableOption "Kitty terminal emulator" // {
+      default = true;
+    };
+  };
+
+  config = lib.mkIf config.j.gui.kitty.enable {
+    programs.kitty.enable = true;
+    xdg.configFile."kitty".source = "${rootPath}/dotfiles-repo/kitty";
+  };
+}
