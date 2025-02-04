0df0e7683c0180e42f8f088d1d9dbc1acec56a10
jazzpi <jasper@mezzo.de>
2025-02-04 02:03:00 +0100

    Set $TERMINAL=kitty

--- home/gui/kitty.nix
+++ home/gui/kitty.nix
@@ -15,5 +15,8 @@
   config = lib.mkIf config.j.gui.kitty.enable {
     programs.kitty.enable = true;
     xdg.configFile."kitty".source = "${rootPath}/dotfiles-repo/kitty";
+    home.sessionVariables = {
+      TERMINAL = "kitty";
+    };
   };
 }
