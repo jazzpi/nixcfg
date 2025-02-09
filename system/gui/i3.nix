9ed31d8e590ea86d08fb174f8b47c7076e20eb87
jazzpi <jasper@mezzo.de>
2025-02-09 17:43:53 +0100

    Disable XFCE package clutter

--- system/gui/i3.nix
+++ system/gui/i3.nix
@@ -12,6 +12,17 @@
   };
 
   config = lib.mkIf config.j.gui.i3.enable {
+    environment.xfce.excludePackages = with pkgs.xfce; [
+      mousepad
+      parole
+      ristretto
+      xfce4-appfinder
+      xfce4-notifyd
+      xfce4-screenshooter
+      xfce4-session
+      xfce4-taskmanager
+      xfce4-terminal
+    ];
     services.xserver = {
       enable = true;
       windowManager.i3.enable = true;
