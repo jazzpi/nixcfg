2b369b51bf0b34c73549fbff2febcef138188744
jazzpi <jasper@mezzo.de>
2025-03-05 09:49:55 +0100

    Add dev shell for genasat

---
+++ shells/gena/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            boost.dev
+            libgcrypt
+            systemd.dev
+          ];
+          nativeBuildInputs = [
+            pkgconf
+            pkg-config
+            cmake
+          ];
+          hardeningDisable = [ "all" ];
+        };
+      }
+    );
+}
