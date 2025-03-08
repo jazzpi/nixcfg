24716e1344a1932f866bc57bb160462bc2a051f8
jazzpi <jasper@mezzo.de>
2025-03-08 16:45:51 +0100

    Use unstable

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
