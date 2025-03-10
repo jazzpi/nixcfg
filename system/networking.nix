9987c05f19fb4bde326d251a678a0a6e948e82ba
jazzpi <jasper@mezzo.de>
2025-03-10 22:33:39 +0100

    Fix network config merging

--- system/networking.nix
+++ system/networking.nix
@@ -19,24 +19,18 @@
     };
   };
 
-  config =
-    lib.mkIf config.j.networking.enable {
-      networking.networkmanager.enable = true;
-      # TODO: networking.wireless.enable ?
-      networking.firewall.checkReversePath = false;
+  config = lib.mkIf config.j.networking.enable {
+    networking.networkmanager.enable = true;
+    # TODO: networking.wireless.enable ?
+    networking.firewall.checkReversePath = false;
 
-      # Make /etc/hosts editable for quick hacks -- these will be discarded on
-      # the next NixOS rebuild.
-      networking.extraHosts = "192.168.2.103 books.jazzpi.de";
-    }
-    // lib.mkIf config.j.networking.wireguard {
-      environment.systemPackages = with pkgs; [
-        wireguard-tools
-      ];
-    }
-    // lib.mkIf config.j.networking.can {
-      environment.systemPackages = with pkgs; [
-        can-utils
-      ];
-    };
+    # Make /etc/hosts editable for quick hacks -- these will be discarded on
+    # the next NixOS rebuild.
+    environment.etc.hosts.mode = "0644";
+
+    environment.systemPackages =
+      with pkgs;
+      (lib.optional config.j.networking.wireguard wireguard-tools)
+      ++ (lib.optional config.j.networking.can can-utils);
+  };
 }
