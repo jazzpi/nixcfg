16e40f0b4e57f10f2724ecc68f40c5cea0551483
jazzpi <jasper@mezzo.de>
2025-03-12 16:49:25 +0100

    Add python shell

---
+++ shells/python/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          # TODO: Is there a difference between buildInputs and
+          # nativeBuildInputs for dev shells?
+          nativeBuildInputs = [
+            uv
+            mypy
+          ];
+          hardeningDisable = [ "all" ];
+        };
+      }
+    );
+}
