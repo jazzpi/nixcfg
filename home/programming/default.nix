b1e05c290d865748d65231d14e198a10b1ea745c
jazzpi <jasper@mezzo.de>
2025-03-15 00:34:22 +0100

    Use FHS for VSCode

--- home/programming/default.nix
+++ home/programming/default.nix
@@ -5,6 +5,7 @@
     ./cpp.nix
     ./git.nix
     ./direnv.nix
+    ./vscode.nix
   ];
 
   options.j.programming.enable = lib.mkEnableOption "Programming support" // {
---
+++ home/programming/vscode.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  options.j.programming.code = {
+    enable = lib.mkEnableOption "VSCode" // {
+      default = config.j.programming.enable && config.j.gui.enable;
+    };
+  };
+
+  config = lib.mkIf config.j.programming.code.enable {
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscode.fhs;
+    };
+  };
+}
