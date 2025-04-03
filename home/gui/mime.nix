12dabd8e69e426df229f1996806d48adf5eecefa
jazzpi <jasper@mezzo.de>
2025-04-03 09:59:57 +0200

    Add some MIME associations

--- home/gui/mime.nix
+++ home/gui/mime.nix
@@ -9,9 +9,16 @@
   config = lib.mkIf config.j.gui.mime.enable {
     xdg.mimeApps = {
       enable = true;
+      associations.added = {
+        "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+        "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
+        "text/plain" = [ "org.gnome.TextEditor.desktop" ];
+      };
       defaultApplications = {
         "application/pdf" = [ "sioyek.desktop" ];
         "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+        "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+        "x-scheme-handler/tonsite" = [ "org.telegram.desktop.desktop" ];
       };
     };
   };
