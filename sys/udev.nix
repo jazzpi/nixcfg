14e23f474ee8d84738334a783b6a763d9fda4acf
jazzpi <jasper@mezzo.de>
2025-04-24 00:17:03 +0200

    Add udev rule for ST-LINK v3

--- sys/udev.nix
+++ sys/udev.nix
@@ -21,6 +21,7 @@
       ATTRS{product}=="*CMSIS-DAP*", MODE="664", GROUP="plugdev"
       ATTRS{product}=="*STM32*", MODE="664", GROUP="plugdev"
       ATTRS{product}=="*ST-LINK*", MODE="664", GROUP="plugdev"
+      ATTRS{product}=="*STLINK*", MODE="664", GROUP="plugdev"
       ATTRS{product}=="*CodeWarrior TAP*", MODE="664", GROUP="plugdev"
     '';
     users.users.${host.user.name}.extraGroups = [ "plugdev" ];
