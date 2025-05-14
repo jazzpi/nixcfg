35bc8de3f5155614619c58ef000ad28a2fc1cc93
jazzpi <jasper@mezzo.de>
2025-05-14 19:06:44 +0200

    Add dev flake for Python + Java

---
+++ shells/python-java/flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            uv
+            mypy
+            poetry
+            pre-commit
+            python3Packages.jupyter
+            python3Packages.pip
+            jdk
+          ];
+          hardeningDisable = [ "all" ];
+        };
+      }
+    );
+}
