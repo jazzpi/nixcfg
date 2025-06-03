e0ada531d3a82666a0c375c0bc6ca4762a36dd11
jazzpi <jasper@mezzo.de>
2025-06-03 16:07:42 +0200

    Add dev shell for AbsInt a3

---
+++ shells/a3/flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        fhs = pkgs.buildFHSUserEnv {
+          name = "a3-shell";
+          targetPkgs =
+            pkgs: with pkgs; [
+              brotli.lib
+              cups.lib
+              libGL
+              expat
+              krb5.lib
+              xorg.libICE
+              xorg.libSM
+              xorg.libX11
+              xorg.libxcb
+              xorg.xcbutilwm
+              xorg.xcbutilimage
+              xorg.xcbutilcursor
+              xorg.xcbutilkeysyms
+              xorg.xcbutilrenderutil
+              libxkbcommon
+              wayland
+              zlib
+            ];
+        };
+      in
+      {
+        devShells.default = fhs.env;
+      }
+    );
+}
