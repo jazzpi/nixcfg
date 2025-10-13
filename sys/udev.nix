a18828f87432aa90cf7e97842bd182278a179b73
jazzpi <jasper@mezzo.de>
2025-10-13 15:30:41 +0200

    Add udev rules for CANnectivity devices

--- sys/udev.nix
+++ sys/udev.nix
@@ -9,6 +9,9 @@
     debuggers = lib.mkEnableOption "udev rules for debuggers" // {
       default = true;
     };
+    cannectivity = lib.mkEnableOption "udev rules for CANnectivity devices" // {
+      default = config.j.networking.can;
+    };
     plugdevGID = lib.mkOption {
       type = lib.types.int;
       default = 46; # ID of plugdev group on Ubuntu
@@ -16,18 +19,34 @@
     };
   };
 
-  config = lib.mkIf config.j.udev.debuggers {
-    services.udev.extraRules = ''
-      ATTRS{product}=="*CMSIS-DAP*", MODE="664", GROUP="plugdev"
-      ATTRS{product}=="*STM32*", MODE="664", GROUP="plugdev"
-      ATTRS{product}=="*ST-LINK*", MODE="664", GROUP="plugdev"
-      ATTRS{product}=="*STLINK*", MODE="664", GROUP="plugdev"
-      ATTRS{product}=="DFU in * Mode", MODE="664", GROUP="plugdev"
-      ATTRS{product}=="*CodeWarrior TAP*", MODE="664", GROUP="plugdev"
-    '';
-    users.users.${host.user.name}.extraGroups = [ "plugdev" ];
-    users.groups.plugdev = {
-      gid = config.j.udev.plugdevGID;
-    };
-  };
+  config =
+    let
+      cfg = config.j.udev;
+    in
+    lib.mkMerge [
+      (lib.mkIf cfg.debuggers {
+        services.udev.extraRules = ''
+          ATTRS{product}=="*CMSIS-DAP*", MODE="664", GROUP="plugdev"
+          ATTRS{product}=="*STM32*", MODE="664", GROUP="plugdev"
+          ATTRS{product}=="*ST-LINK*", MODE="664", GROUP="plugdev"
+          ATTRS{product}=="*STLINK*", MODE="664", GROUP="plugdev"
+          ATTRS{product}=="DFU in * Mode", MODE="664", GROUP="plugdev"
+          ATTRS{product}=="*CodeWarrior TAP*", MODE="664", GROUP="plugdev"
+        '';
+        users.users.${host.user.name}.extraGroups = [ "plugdev" ];
+        users.groups.plugdev = {
+          gid = cfg.plugdevGID;
+        };
+      })
+      (lib.mkIf cfg.cannectivity {
+        services.udev.extraRules = ''
+          # Load gs_usb when a CANnectivity device is plugged in
+          ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="1209", ATTR{idProduct}=="ca01", RUN+="/sbin/modprobe -b gs_usb" MODE="660", GROUP="plugdev", TAG+="uaccess"
+          # Bind CANnectivity devices to gs_usb driver (when gs_usb is loaded)
+          SUBSYSTEM=="drivers", ENV{DEVPATH}=="/bus/usb/drivers/gs_usb", ATTR{new_id}="1209 ca01 ff"
+          # Set permissions when a CANnectivity device in DFU mode is plugged in
+          ATTR{idVendor}=="1209", ATTR{idProduct}=="ca02", MODE="660", GROUP="plugdev", TAG+="uaccess"
+        '';
+      })
+    ];
 }
