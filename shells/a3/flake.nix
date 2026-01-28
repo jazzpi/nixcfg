2b4ad86fc4e6d3a46a15660fe2ab1ff2be252e78
jazzpi <jasper@mezzo.de>
2026-01-28 10:45:27 +0100

    Update dev shells to nixpkgs 25.11

--- shells/a3/flake.nix
+++ shells/a3/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
@@ -15,7 +15,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        fhs = pkgs.buildFHSUserEnv {
+        fhs = pkgs.buildFHSEnv {
           name = "a3-shell";
           targetPkgs =
             pkgs: with pkgs; [
--- shells/avr/flake.nix
+++ shells/avr/flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
     {
-      self,
       nixpkgs,
       flake-utils,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
