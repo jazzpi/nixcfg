c9b6aebe15ddfaf69f163b2a5c9335f34380955d
jazzpi <jasper@mezzo.de>
2026-02-23 14:24:07 +0100

    fix: actually respect gui.uwsm.compositors.*.extraArgs

--- modules/sys/gui/uwsm.nix
+++ modules/sys/gui/uwsm.nix
@@ -3,25 +3,39 @@ with lib;
 {
   options = {
     j.gui.uwsm.compositors = mkOption {
-      type = with types; attrsOf (attrsOf str);
+      type =
+        with types;
+        attrsOf (submodule {
+          options = {
+            name = mkOption {
+              type = str;
+              description = "A user-friendly name for the compositor.";
+            };
+            binPath = mkOption {
+              type = path;
+              description = "The path to the compositor binary.";
+            };
+            extraArgs = mkOption {
+              type = listOf str;
+              default = [ ];
+              description = "Additional arguments to pass to the compositor.";
+            };
+          };
+        });
       default = { };
-      description = ''
-        Additional Wayland compositors to add to UWSM.
-        The key is the compositor ID, the value is an attribute set with the following keys:
-        - name: A user-friendly name for the compositor.
-        - binPath: The path to the compositor binary.
-      '';
+      description = "Additional Wayland compositors to add to UWSM.";
     };
   };
   config = mkIf config.j.gui.uwsm.enable {
     programs.uwsm = {
       enable = true;
       waylandCompositors = (
-        (mapAttrs (key: value: {
+        mapAttrs (key: value: {
           prettyName = value.name;
           comment = "UWSM-managed ${value.name}";
           binPath = value.binPath;
-        }) config.j.gui.uwsm.compositors)
+          extraArgs = value.extraArgs;
+        }) config.j.gui.uwsm.compositors
       );
     };
   };
