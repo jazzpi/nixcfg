ee95b0c0bdaabb1a902bef72a152252e960a318d
jazzpi <jasper@mezzo.de>
2026-06-26 17:07:59 +0200

    feat: replace SDDM with GDM

--- modules/sys/gui/display-manager.nix
+++ modules/sys/gui/display-manager.nix
@@ -14,29 +14,41 @@
   };
   config = lib.mkIf config.j.gui.enable (
     let
-      astronaut = (
-        pkgs.sddm-astronaut.override {
-          themeConfig = {
-            background = "${paths.store.wallpapers}/default.jpg";
-          };
-          embeddedTheme = "black_hole";
-        }
-      );
+      background = "${paths.store.wallpapers}/default.jpg";
     in
     {
       services.displayManager = {
         enable = true;
         defaultSession = config.j.gui.displayManager.defaultSession;
-        sddm = {
-          enable = true;
-          theme = "sddm-astronaut-theme";
-          extraPackages = [
-            astronaut
-          ];
-        };
+        gdm.enable = true;
       };
-      environment.systemPackages = [
-        astronaut
+      nixpkgs.overlays = [
+        (final: prev: {
+          gnome-shell = prev.gnome-shell.overrideAttrs (old: {
+            postInstall = (old.postInstall or "") + ''
+              theme=$out/share/gnome-shell/gnome-shell-theme.gresource
+              tmp=$(mktemp -d)
+              ( cd "$tmp"
+                for r in $(gresource list "$theme"); do
+                  gresource extract "$theme" "$r" > "$(basename "$r")"
+                done
+                cp ${background} my-bg.png
+                for css in gnome-shell-dark.css gnome-shell-light.css; do
+                  printf '%s\n' \
+                    "#lockDialogGroup { background: url('resource:///org/gnome/shell/theme/my-bg.png'); background-size: cover; background-repeat: no-repeat; }" \
+                    >> "$css"
+                done
+                { echo '<?xml version="1.0" encoding="UTF-8"?>'
+                  echo '<gresources><gresource prefix="/org/gnome/shell/theme">'
+                  for f in *; do echo "  <file>$f</file>"; done
+                  echo '</gresource></gresources>'
+                } > theme.gresource.xml
+                glib-compile-resources --target="$theme" theme.gresource.xml
+              )
+              rm -rf "$tmp"
+            '';
+          });
+        })
       ];
     }
   );
