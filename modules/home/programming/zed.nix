a212b1900902377d5dc7168fd528f4539b959305
jazzpi <jasper@mezzo.de>
2026-09-01 18:17:16 +0200

    feat(zed): basic config

--- modules/home/programming/zed.nix
+++ modules/home/programming/zed.nix
@@ -12,7 +12,47 @@
   config = lib.mkIf config.j.programming.zed.enable {
     programs.zed-editor = {
       enable = true;
-      extraPackages = with pkgs; [ nil ];
+      extraPackages = with pkgs; [
+        nil
+        asm-lsp
+        neocmakelsp
+        black
+      ];
+      extensions = [
+        "nix"
+        "toml"
+        "docker-compose"
+        "dockerfile"
+        "one-dark-pro"
+        "xml"
+        "make"
+        "neocmake"
+        "assembly"
+        "linkerscript"
+        "desktop"
+        "wgsl"
+        "editorconfig"
+      ];
+      userSettings = {
+        theme = "One Dark Pro";
+        buffer_font_family = "Meslo LG S DZ";
+        buffer_font_size = 12;
+        vim_mode = true;
+        telemetry = {
+          diagnostics = false;
+          metrics = false;
+        };
+        format_on_save = "on";
+        relative_line_numbers = true;
+        languages.Python.formatter.external = {
+          command = "black";
+          arguments = [
+            "--stdin-filename"
+            "{buffer_path}"
+            "-"
+          ];
+        };
+      };
     };
   };
 }
