11b4f7af05334c29b14fc27a075461ee6daf9f0b
jazzpi <jasper@mezzo.de>
2026-09-15 11:39:17 +0200

    feat(magit): expand more sections by default

--- modules/home/programming/doom.d/config.el
+++ modules/home/programming/doom.d/config.el
@@ -1,3 +1,9 @@
 ;;; config.el -*- lexical-binding: t; -*-
 
 (setq confirm-kill-emacs nil)
+
+(after! magit-section
+  (setq magit-section-initial-visibility-alist
+        (append '((untracked . show) (recent . show)
+                  (unpushed . show) (unpulled . show))
+                magit-section-initial-visibility-alist)))
